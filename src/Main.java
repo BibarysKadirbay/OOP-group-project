d605c08745b586008ff44115ea263d7b0543d2e8
BibarysKadirbay <bibariskadirbai@gmail.com>
2025-01-22 14:06:11 +0500

    bibarys

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("HEllo world");
-        System.out.println("Erabot");
-        System.out.println("arrl");
+        Person
     }
 }
