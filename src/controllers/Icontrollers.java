fc3629505293e87c43428853dcddbc475b9e7b30
LuckyEra <erabot00127@gmail.com>
2025-01-23 15:00:28 +0600

    Ersultan

--- src/controllers/Icontrollers.java
+++ src/controllers/Icontrollers.java
@@ -1,7 +1,8 @@
 package controllers.interface;
 
 public interface Icontrollers {
-    String createAccount(String name, String surname, String);
+    String createAccount(String name, String surname, String TelegramTag);
     String findUserById(int id);
     String getListAccount();
+    String registerUserWithDetails(int barcode, String name, String surname, String tgNickname, String group, String city, String gender, int age);
 }
