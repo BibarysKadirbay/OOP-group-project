74026dac8a435866bcc134baa40a280e87ad5ca7
david_khamitskiy <davidxqd58@gmail.com>
2025-01-23 18:01:01 +0600

    david

--- src/controllers/Icontrollers.java
+++
@@ -1,7 +0,0 @@
-package controllers.interface;
-
-public interface Icontrollers {
-    String createAccount(String name, String surname, String);
-    String findUserById(int id);
-    String getListAccount();
-}
